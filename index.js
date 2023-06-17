f09c04825396750e0c2c569efb0c38491e46f258
Alex Washington <alexthewebdev@gmail.com>
2023-06-17 13:22:33 -0400

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         alert("Saw that black weiner and couldn't resist, eh?");
       });
       $("#answer").click(function(){
-        alert("Congratulations! You re-established the connection and opened the lines of communication. Locate the moment of the final correspondence to release The Shard.");
+        alert("Great work, Z64-JRK. You successfully synced with The Sacred Timeline, re-established the connection, and opened the lines of communication. Locate the moment of the final correspondence to collect the Shard. For All Time. Always!");
       });
 })
 
